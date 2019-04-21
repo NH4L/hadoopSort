4df78373bec84b79452824b13a659e2bd3fdf681
NH4L <44610189+NH4L@users.noreply.github.com>
2019-04-21 15:22:24 +0800

    排序算法

--- Sort.java
+++ Sort.java
@@ -100,6 +100,7 @@ public class Sort {
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
 	}
 
 }
